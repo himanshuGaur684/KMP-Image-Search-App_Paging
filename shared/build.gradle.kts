31dd04532ec4dfbae9fa85f3e58ce36c391e0d88
Himanshu <himanshugaur684@gmail.com>
2025-02-13 12:40:58 +0530

    Initial Project

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,9 +1,12 @@
+
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.kotlinx.ksp)
+    alias(libs.plugins.observable.kmp.viewmodel)
 }
 
 kotlin {
@@ -11,12 +14,11 @@ kotlin {
         compilations.all {
             compileTaskProvider.configure {
                 compilerOptions {
-                    jvmTarget.set(JvmTarget.JVM_1_8)
+                    jvmTarget.set(JvmTarget.JVM_17)
                 }
             }
         }
     }
-    
     listOf(
         iosX64(),
         iosArm64(),
@@ -29,10 +31,18 @@ kotlin {
     }
 
     sourceSets {
+        all {
+            languageSettings.optIn("kotlinx.cinterop.ExperimentalForeignApi")
+
+        }
         androidMain.dependencies{
             implementation(libs.ktor.client.android)
+            implementation(libs.koin.androidx.compose)
+            implementation(libs.koin.android)
         }
         commonMain.dependencies {
+
+            implementation(libs.kotlinx.coroutines)
 
             // koin
             implementation(libs.koin.core)
@@ -47,9 +57,12 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(libs.androidx.lifecycle.viewmodel)
 
-            //put your multiplatform dependencies here
-            implementation(libs.paging.compose.common)
+            // Paging
             implementation(libs.paging.common)
+
+            // KMP Observable ViewModel
+            api(libs.kmp.observableviewmodel.core)
+
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.ios)
@@ -69,7 +82,12 @@ android {
         minSdk = 24
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
+
+kotlin.sourceSets.all {
+    languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
+}
+
